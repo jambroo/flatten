883a0ef64f75942803f345a7050dd32e55c0137f
James Brooking <jambroo@gmail.com>
2017-10-04 10:56:41 +0100

    Exporting flatten function for testing.

--- flatten.js
+++ flatten.js
@@ -1,15 +1,18 @@
 'use strict'
 
-// function
+/*
+  This flatten function takes a multi-dimensional array and returns a flattened
+  array. Example: flatten([1, 2, [3], 4]) results in [1, 2, 3, 4]
+*/
 const flatten = (array) => {
   return array.reduce((a, element)=> {
-    console.log({a,element})
-    return a.concat(element)}, []);
+    return a.concat(element);
+  }, []);
 }
-
 
 
 // test
 // flatten([1, 2, [3], 4]) === [1, 2, 3, 4]
 
-console.log(flatten([1, 2, [3], 4]));
+
+module.exports = flatten
