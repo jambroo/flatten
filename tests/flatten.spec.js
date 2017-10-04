685101653b28b79ca8c3f570997767ce825dadcb
James Brooking <jambroo@gmail.com>
2017-10-04 11:14:07 +0100

    Add more tests.

--- tests/flatten.spec.js
+++ tests/flatten.spec.js
@@ -8,4 +8,17 @@ describe('flatten function', () => {
   it('should export a function', () => {
     expect(flatten).to.be.a('function')
   })
+
+  it('given an empty array should result in an empty array', () => {
+    expect(flatten([])).to.eql([])
+  })
+
+  it('given a single-dimensional array should result in the same array', () => {
+    expect(flatten([1, 2, 3])).to.eql([1, 2, 3])
+  })
+
+  it('given a multi-dimensional array should result in the same array flattened', () => {
+    expect(flatten([1, 2, [3]])).to.eql([1, 2, 3])
+    expect(flatten([[1,2,[3]],4])).to.eql([1, 2, 3, 4])
+  })
 })
